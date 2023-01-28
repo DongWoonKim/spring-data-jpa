0a5053621cea063e29d2ee3b0e1c21a8cf57d38b
DongWoonKim <01030985843z@naver.com>
2023-01-28 18:29:54 +0900

    나머지 기능들 : 네이티브 쿼리 완료

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -362,4 +362,32 @@ class MemberRepositoryTest {
 
     }
 
+    @Test
+    public void nativeQuery() {
+
+        // given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member(0, "m1", teamA);
+        Member m2 = new Member(0, "m2", teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        // when
+        Page<MemberProjection> result = memberRepository.findByNativeProjection(PageRequest.of(0, 10));
+        List<MemberProjection> content = result.getContent();
+
+        // then
+        for (MemberProjection memberProjection : content) {
+            System.out.println("memberProjection.getUsername() = " + memberProjection.getUsername());
+            System.out.println("memberProjection.getTeamName() = " + memberProjection.getTeamName());
+        }
+
+
+    }
+
 }
